f7e22a1487155e460e2b58f71777bd721bca1506
quigley-c <carson.ll.quigley@gmail.com>
2021-09-12 16:57:00 -0400

    fancy commentms?

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,3 +1,5 @@
+/* made with love by Carson Quigley */
+
 package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
@@ -8,17 +10,25 @@ import kotlinx.android.synthetic.main.activity_main.*
 class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        /*I almost defined another onCreate because I didn't realize
+        there was already one here.. */
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        //the magic happens with the OnClickListener.
         goButton.setOnClickListener(this)
     }
 
 
     override fun onClick(view: View?)   {
+    /* when <button> do (->) <thing> */
         when(view)  {
             goButton -> textView.setText("Button Pressed!")
+
+            //maybe I'll add a second button that does something else..
         }
 
+    // I managed to launch this app on my own phone which is pretty cool imo
     }
 }
