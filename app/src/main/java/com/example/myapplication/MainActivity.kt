91d4c055f81bb77c48d74aad26218201508f2989
quigley-c <carson.ll.quigley@gmail.com>
2021-09-12 16:46:59 -0400

    layout, onClick button, text

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        goButton.setOnClickListener(this)
+    }
+
+
+    override fun onClick(view: View?)   {
+        when(view)  {
+            goButton -> textView.setText("Button Pressed!")
+        }
+
     }
 }
